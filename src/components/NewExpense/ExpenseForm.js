8673b156b34bc5d532538fa9963c6b2fbcf04f29
gkenenitz <gkenenitz@apple.com>
2021-10-28 09:40:05 -0400

    Two way binding: we can update the state based on user input, but we can also update the DOM via state: set fields to empty string after form submit, and use 'value' in html elements

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -35,6 +35,10 @@ const ExpenseForm = () => {
     }
 
     console.log(expenseData);
+
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
   };
 
   return (
@@ -42,15 +46,15 @@ const ExpenseForm = () => {
     <div className="new-expense__controls">
       <div className="new-expense__control">
         <label>Title</label>
-        <input type='text' onChange={titleChangeHandler}/>
+        <input type='text' onChange={titleChangeHandler} value={enteredTitle}/>
       </div>
       <div className="new-expense__control">
         <label>Amount</label>
-        <input type='number' onChange={amountChangeHandler} min="0.01" step="0.01" />
+        <input type='number' onChange={amountChangeHandler} min="0.01" step="0.01"  value={enteredAmount}/>
       </div>
       <div className="new-expense__control">
         <label>Date</label>
-        <input type='date' onChange={dateChangeHandler} min="2019-01-01" max="2022-12-31"/>
+        <input type='date' onChange={dateChangeHandler} min="2019-01-01" max="2022-12-31" value={enteredDate}/>
       </div>
     </div>
 
